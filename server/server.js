e7d100a0bb249ecf83e775c13ef7f0f73c2101bc
quangpmm <quangpmm@nodo.vn>
2024-09-22 23:46:21 +0700

    update 22092024

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const cluster = require('cluster');
+const os = require('os');
 const express = require("express"); // import express, không dùng babel
 require("dotenv").config();
 const {
@@ -14,27 +16,47 @@ const dbConnect = require("./config/dbconnect");
 const initRoutes = require("./routes/index.js");
 const cookieParser = require("cookie-parser");
 
-const app = express();
 
-// CORS
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ['POST', 'PUT', 'GET', 'DELETE']
-}))
 
-const port = process.env.PORT || 8888;
-app.use(express.json()); // để express đọc được data từ client là json
-app.use(express.urlencoded({ extended: true })); // để client gửi các kiểu ... thì server dùng được
-app.use(cookieParser());
+if(cluster.isMaster) {
+  // Cân bằng tải cho các worker CPU
+  const numCPUs = os.cpus().length; // Lấy số lượng core CPU
+  console.log(`Master ${process.pid} is running`);
+  // Tạo một worker cho mỗi core CPU
+  for(let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// connect DB
-dbConnect();
-initRoutes(app); // tạo routes
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    console.log('Forking a new worker...');
+    cluster.fork(); // Tự động tạo lại worker mới nếu một worker bị die
+  });
+}
+else {
+  const app = express();
 
-app.use("/", (req, res) => {
-  res.send("server on");
-});
+  // CORS
+  app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['POST', 'PUT', 'GET', 'DELETE']
+  }))
 
-app.listen(port, () => {
-  console.log("Server running on the port: ", port);
-});
+  const port = process.env.PORT || 8888;
+  app.use(express.json()); // để express đọc được data từ client là json
+  app.use(express.urlencoded({ extended: true })); // để client gửi các kiểu ... thì server dùng được
+  app.use(cookieParser());
+
+  // connect DB
+  dbConnect();
+  initRoutes(app); // tạo routes
+
+  app.use("/", (req, res) => {
+    res.send("server on");
+  });
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} is running on the port: ${port}`);
+  });
+}
+
